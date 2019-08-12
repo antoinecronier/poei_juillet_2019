4a615c7c18c3a6d8dbe326ee78e6ab9fdbb27c27
Antoine Cronier <antoine.cronier@tactfactory.com>
2019-08-12 11:36:28 +0200

    apply code quality

--- src/main/java/com/poei_juillet_2019/mysql/MySqlApplication.java
+++ src/main/java/com/poei_juillet_2019/mysql/MySqlApplication.java
@@ -1,9 +1,21 @@
 package com.poei_juillet_2019.mysql;
 
-public class MySqlApplication {
+/**
+ *
+ * @author antoine.cronier
+ *
+ */
+public final class MySqlApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+    private MySqlApplication() {
 
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
